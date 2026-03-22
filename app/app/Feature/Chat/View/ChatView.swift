ec5c8507a79e561059535efc7d0f08e7bc761d05
openmpy <suhwan@kakao.com>
2026-03-23 03:36:25 +0900

    feat: 채팅 뷰 디자인 뼈대 작성

--- app/app/Feature/Chat/View/ChatView.swift
+++ app/app/Feature/Chat/View/ChatView.swift
@@ -4,8 +4,54 @@ struct ChatView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("채팅")
+            ScrollView(showsIndicators: false) {
+                LazyVStack(spacing: 10) {
+                    ForEach(0..<1000) { i in
+                        HStack(spacing: 12) {
+                            Image(systemName: "person.fill")
+                                .font(.title)
+                                .frame(width: 55, height: 55)
+                                .foregroundColor(Color(.systemGray6))
+                                .background(Color(.systemGray4))
+                                .clipShape(Circle())
+
+                            VStack(alignment: .leading, spacing: 5) {
+                                HStack {
+                                    Text("닉네임")
+                                        .font(.headline.bold())
+
+                                    Spacer()
+
+                                    Text("방금 전")
+                                        .font(.caption)
+                                        .foregroundColor(Color(.systemGray))
+                                }
+
+                                HStack(alignment: .center) {
+                                    Text(i % 2 == 0
+                                         ? "메시지메시지메시지메시지메시지메시지메시"
+                                         : "메시지메시지메시지메시지메시지메시지메시지메시지메시지메시지메시지메시지메시지메시"
+                                    )
+                                    .lineLimit(2)
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(.systemGray))
+
+                                    Spacer()
+
+                                    Text("99")
+                                        .font(.caption2)
+                                        .foregroundColor(Color(.systemBackground))
+                                        .padding(.horizontal, 6)
+                                        .padding(.vertical, 2)
+                                        .background(Color(.systemGray4))
+                                        .clipShape(Capsule())
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+                    }
+                }
             }
             .navigationTitle("채팅")
             .navigationBarTitleDisplayMode(.inline)
@@ -31,5 +77,5 @@ struct ChatView: View {
 }
 
 #Preview {
-    RecentView()
+    ChatView()
 }
